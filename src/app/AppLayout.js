3f7705188cc256f9f0caa6e05cef90348cda8ead
masteroffluff <chris.chapman@linuxmail.org>
2023-11-12 21:08:30 +0000

    trying to fix the poor svg file

--- src/app/AppLayout.js
+++ src/app/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import Search from "../features/search/Search";
-import logo from './logo.svg';
+import logo from '../logo.svg';
 
 import PreviousSubreddits from "../features/previousSubreddits/PreviousSubreddits";
 

d9560b9adc7a929f8ad114b21dbdf3a02478e28e
masteroffluff <chris.chapman@linuxmail.org>
2024-06-05 14:26:06 +0100

    added debugging to RedditVideo

--- src/components/media/RedditVideo.js
+++ src/components/media/RedditVideo.js
@@ -1,64 +1,76 @@
 import React from "react";
-//import ReactHlsPlayer from "react-hls-player";
 import VideoJS from "./VideoJS";
 import videojs from "video.js";
 
-// can't get the sound to work :(
-export default function RedditVideo({media}){
-     //const videoURL = media.reddit_video.fallback_url
-    const playerRef = React.useRef(null); 
-    if (!media.reddit_video){return <div>video error</div>}
-    const {hls_url, width, height} = media.reddit_video
- /*   return (
-        <>
-            <ReactHlsPlayer
-                src={hls_url}
-                autoPlay={false}
-                controls={true}
-                width="500px"
-                height="auto"
-            />
+export default function RedditVideo({ media }) {
+    const playerRef = React.useRef(null);
 
-        </>
-    ) */
-    
+    if (!media.reddit_video) {
+        return <div>video error</div>;
+    }
+
+    const { hls_url, width, height } = media.reddit_video;
 
     const videoJsOptions = {
-      //preload:'none',
-      autoplay: false,
-      controls: true,
-      responsive: true,
-      fluid:false,
-/*       width:width,
-      height:height, */
-      sources: [{
-        src: hls_url,
-        type: "application/x-mpegURL"
-      }]
+        autoplay: false,
+        controls: true,
+        responsive: true,
+        fluid: false,
+        sources: [{
+            src: hls_url,
+            type: "application/x-mpegURL"
+        }]
     };
-  
-    const handlePlayerReady = (player) => {
-      playerRef.current = player;
-  
-      // You can handle player events here, for example:
-      player.on('waiting', () => {
-        videojs.log('player is waiting');
-      });
-  
-      player.on('dispose', () => {
-        videojs.log('player will dispose');
-      });
-    };
-    
-    
 
+    const handlePlayerReady = (player) => {
+        playerRef.current = player;
+
+        player.on('waiting', () => {
+            videojs.log('player is waiting');
+        });
+
+        player.on('dispose', () => {
+            videojs.log('player will dispose');
+        });
+
+        // Add event listeners for debugging
+        player.on('error', () => {
+            const error = player.error();
+            videojs.log('Error:', error);
+        });
+
+        player.on('loadstart', () => {
+            videojs.log('Loading started');
+        });
+
+        player.on('loadeddata', () => {
+            videojs.log('Data loaded');
+        });
+
+        player.on('loadedmetadata', () => {
+            videojs.log('Metadata loaded');
+        });
+
+        player.on('canplay', () => {
+            videojs.log('Can play');
+        });
+
+        player.on('playing', () => {
+            videojs.log('Playing');
+        });
+
+        player.on('pause', () => {
+            videojs.log('Paused');
+        });
+
+        player.on('ended', () => {
+            videojs.log('Ended');
+        });
+    };
 
     return (
-      <div className="reddit-video video">
-       {/*  <VideoJS options={videoJsOptions} onReady={handlePlayerReady} width={width} height={height} /> */}
-       <VideoJS options={videoJsOptions} onReady={handlePlayerReady} width={width} height={height} />
-      </div>
+        <div className="reddit-video video">
+            <VideoJS options={videoJsOptions} onReady={handlePlayerReady} width={width} height={height} />
+        </div>
     );
-
 }
-

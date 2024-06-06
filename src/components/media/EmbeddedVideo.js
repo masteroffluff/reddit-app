c17959f2bdd6c92c2e85bee65342b70d17f7d1f1
masteroffluff <chris.chapman@linuxmail.org>
2024-06-06 15:28:52 +0100

    fixed thumbnails

--- src/components/media/EmbeddedVideo.js
+++ src/components/media/EmbeddedVideo.js
@@ -2,12 +2,14 @@ import React, {useState} from "react";
 
 
 
-export default function EmbeddedVideo ({media}){
-    const {html, title, thumbnail_url, height,width} = media.oembed;
+export default function EmbeddedVideo ({media,thumbnail}){
+    const {html, title , height,width, thumbnai_width, thumbnail_height} = media.oembed;
     //const html = (dummyVideo)
     const [loadVideo, setLoadVideo]= useState(false);
-
-   
+    const thumbnailToUse = thumbnail;
+    const image_width = thumbnai_width||width;
+    const image_height = thumbnai_width||height;
+    
     let src=html.match(/(?<=src=").*"/)
     src = String(src).split('"')[0]
     //return <p>{src}</p>
@@ -20,7 +22,7 @@ export default function EmbeddedVideo ({media}){
         return (
             <>
             <div className="video live-video" style={{paddingBottom:paddingPercentage}}>
-                <iframe src={src} frameborder="0" title={title} allowFullScreen></iframe>
+                <iframe src={src} height={image_height} width={image_width} frameborder="0" title={title} allowFullScreen></iframe>
                 
             </div>
                 <button onClick={handleClick}>Unload</button>
@@ -28,8 +30,9 @@ export default function EmbeddedVideo ({media}){
     }else{
         return(
             <div className="video visualMediaContainer">
-                <img src={thumbnail_url} className="image" onClick={handleClick} alt={title} />
+                <img src={thumbnailToUse} className="image" height={image_height} width={image_width} onClick={handleClick} alt={title} />
             </div>
+
         )
     }
 
